aabf7d30dde7c114103e45c54d8d1c0110c79a15
mchouet <mathias.chouet@inria.fr>
2021-04-20 12:02:45 +0200

    Add R example

---
+++ examples/post/run.R
@@ -0,0 +1,31 @@
+
+API_URL <- "https://my-api.plantnet.org/v2/identify/all?"
+
+key <- "" # Your API key here
+
+lang <- "fr"
+includeRelatedImages <- FALSE # try TRUE
+
+URL <- paste0(API_URL,
+        "lang=", lang,
+        "&include-related-images=", includeRelatedImages,
+        "&api-key=", key)
+
+image_1 <- "../data/image_1.jpeg"
+image_2 <- "../data/image_2.jpeg"
+
+data <- list(
+    "images" = httr::upload_file(image_1),
+    "images" = httr::upload_file(image_2),
+    "organs" = "flower",
+    "organs" = "leaf"
+)
+
+response <- httr::POST(URL, body=data, encode="multipart")
+
+status <- response$status_code
+message(status)
+
+result <- httr::content(response, as = 'parsed')
+
+message(result)

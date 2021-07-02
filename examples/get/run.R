af3addeb8761d668682fdf0e5a585c2a7becc54c
mchouet <mathias.chouet@inria.fr>
2021-07-02 09:49:26 +0200

    Add R GET example

---
+++ examples/get/run.R
@@ -0,0 +1,30 @@
+
+API_URL <- "https://my-api.plantnet.org/v2/identify/all?"
+key <- "" # Your API key here
+imageURL <- "" # A JPG image URL here
+lang <- "fr"
+organs <- "flower"
+includeRelatedImages <- FALSE # try TRUE
+
+URLencoded <- sapply(imageURL, FUN = URLencode, reserved = TRUE, repeated = TRUE)
+
+URL <- paste0(API_URL,
+        "images=", paste(URLencoded, collapse = "&images="),
+        "&organs=", paste(organs, collapse = "&organs="),
+        "&lang=", lang,
+        "&include-related-images=", includeRelatedImages,
+        "&api-key=", key)
+
+message(URL)
+
+# Hit the API
+response <- httr::GET(URL)
+
+# Check the status
+status <- response$status_code
+message(status)
+
+# the status is 200 so let's parse the content of the response
+prediction <- httr::content(response, as = 'parsed')
+
+message(prediction)
